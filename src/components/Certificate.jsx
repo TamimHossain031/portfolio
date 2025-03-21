eac5e3bfc41883200ac1b46031e5ec6a3d84797c
Tamim Hossain <tamimhossain031@gmail.com>
2025-03-21 23:33:41 +0600

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,5 +1,6 @@
 import Head from './HeadOfData.jsx';
 import Cr from '../assets/Snip - Settings freeCodeCamp.org Mozilla Firefo.png';
+import Cr2 from '../assets/Screenshot_2.png';
 export default function Certificate() {
   return (
     <div>
@@ -8,6 +9,8 @@ export default function Certificate() {
         <p className='text-left'>1 . Data Structure & Algorithm</p>
         <img src={Cr} alt="Certificate" />
         <p className='text-left pt-[20px]'>2. React.js - LWS (Learn with Sumit)</p>
+        <p className='text-left pt-[20px]'>3. Node.js & Express.js </p>
+         <img src={Cr2} alt="Certificate" />
       </div>
     </div>
   );
